fdf67a14f2add87b9f17c1ac2040f2e11a1ab9f1
Chesedd <89045380759@mail.ru>
2026-01-14 10:34:40 +0300

    Add rename and delete in context menu for boxes

--- ProjectScreen/CollapsibleBox.py
+++ ProjectScreen/CollapsibleBox.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
-from PyQt6.QtGui import *
+from PyQt6.QtGui import QAction
 
 class CollapsibleBox(QWidget):
     def __init__(self, title="", parent=None):
@@ -56,6 +56,20 @@ class CollapsibleBox(QWidget):
         self.contentLayout.setContentsMargins(10, 10, 10, 10)
         self.contentArea.setWidget(self.contentWidget)
 
+
+    def contextMenuEvent(self, a0):
+        menu = QMenu(self)
+
+        renameAction = QAction("Rename", self)
+        renameAction.triggered.connect(self.showRenameDialog)
+        menu.addAction(renameAction)
+
+        deleteAction = QAction("Delete", self)
+        deleteAction.triggered.connect(self.showDeleteDialog)
+        menu.addAction(deleteAction)
+
+        menu.exec(a0.globalPos())
+
     def onToggled(self, checked):
         if checked:
             self.toggleButton.setText("► " + self.toggleButton.text()[2:])
@@ -77,3 +91,81 @@ class CollapsibleBox(QWidget):
             child = self.contentLayout.takeAt(0)
             if child.widget():
                 child.widget().deleteLater()
+
+    def showRenameDialog(self):
+        dialog = RenameDialog(self)
+        dialog.boxRenamed.connect(self.renameBox)
+        dialog.exec()
+
+    def renameBox(self, newTitle):
+        self.toggleButton.setText("▼ " + newTitle)
+
+    def showDeleteDialog(self):
+        dialog = DeleteDialog(self)
+        dialog.boxDelete.connect(self.deleteBox)
+        dialog.exec()
+
+    def deleteBox(self):
+        self.deleteLater()
+
+class RenameDialog(QDialog):
+    boxRenamed = pyqtSignal(str)
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Rename box")
+        newNameText = QLabel("Insert new title")
+        self.newNameBar = QLineEdit()
+        self.newNameParams = QWidget()
+        newNameLayout = QHBoxLayout(self.newNameParams)
+        newNameLayout.addWidget(newNameText)
+        newNameLayout.addWidget(self.newNameBar)
+
+        self.buttons = QWidget()
+        buttonsLayout = QHBoxLayout(self.buttons)
+        ok_btn = QPushButton("OK")
+        ok_btn.clicked.connect(self.on_ok_clicked)
+        cancel_btn = QPushButton("Cancel")
+        cancel_btn.clicked.connect(self.reject)
+        buttonsLayout.addWidget(ok_btn)
+        buttonsLayout.addWidget(cancel_btn)
+
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.newNameParams)
+        self.mainLayout.addWidget(self.buttons)
+        self.setLayout(self.mainLayout)
+
+    def on_ok_clicked(self):
+        self.boxRenamed.emit(self.newNameBar.text())
+        self.accept()
+
+class DeleteDialog(QDialog):
+    boxDelete = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Delete box")
+        newNameText = QLabel("Are you sure?")
+
+        self.buttons = QWidget()
+        buttonsLayout = QHBoxLayout(self.buttons)
+        ok_btn = QPushButton("OK")
+        ok_btn.clicked.connect(self.on_ok_clicked)
+        cancel_btn = QPushButton("Cancel")
+        cancel_btn.clicked.connect(self.reject)
+        buttonsLayout.addWidget(ok_btn)
+        buttonsLayout.addWidget(cancel_btn)
+
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(newNameText)
+        self.mainLayout.addWidget(self.buttons)
+        self.setLayout(self.mainLayout)
+
+    def on_ok_clicked(self):
+        self.boxDelete.emit()
+        self.accept()
